98f00b8326597a22ef15690c1508a4a0f351f156
yeahseul <yeah2114@naver.com>
2020-10-22 16:33:20 +0900

    del

--- js/cart.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-   
-     $("#check_all").click(function(){
-        var chk = $(this).is(":checked");//.attr('checked');
-        if(chk) $(".cart_table input").prop('checked', true);
-        else  $(".cart_table input").prop('checked', false);
-    });
-    
-});
